a73b2ff1f88362e5be8a75555506bb004c8b5d7d
Harsha C K <harshack.21kumar@gmail.com>
2026-07-27 22:57:27 +0530

    Add personal information input program

---
+++ 01_personal_information.cpp
@@ -0,0 +1,19 @@
+// program 1
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int age;
+    string branch;
+    cout << "Enter your name:";
+    cin >> name;
+    cout << "Enter your age:";
+    cin >> age;
+    cout << "Enter your branch:";
+    cin >> branch;
+    cout << name << endl;
+    cout << age << endl;
+    cout << branch;
+    return 0;
+}
