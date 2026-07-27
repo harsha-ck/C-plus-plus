576bfe3118a5567ce4cc7ea67baa57e1a16f6cdd
Harsha C K <harshack.21kumar@gmail.com>
2026-07-27 23:09:18 +0530

    Add simple calculator program in C++

---
+++ 02_simple_calculator.cpp
@@ -0,0 +1,14 @@
+// program 2
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "Enter two numbers:";
+    cin >> x >> y;
+    cout << "Sum:" << x + y << endl;
+    cout << "Defference:" << x - y << endl;
+    cout << "Product:" << x * y;
+
+    return 0;
+}
