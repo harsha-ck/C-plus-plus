5d2d4b82c52bc486b3e515e4f42b89983f4fbd32
Harsha C K <harshack.21kumar@gmail.com>
2026-07-28 09:36:40 +0530

    Create 08_pattern_printing.cpp

---
+++ 08_pattern_printing.cpp
@@ -0,0 +1,54 @@
+//square
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter size of square:";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+
+//triangle
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the height of the triangle:";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+
+//inverted triangle
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the height of the triangle:";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j > i; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
