533df5205092a55916d006981c871cca08fbe2e8
Harsha C K <harshack.21kumar@gmail.com>
2026-07-28 08:11:19 +0530

    Create 04_odd_or_even.cpp

---
+++ 04_odd_or_even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "enter a  number:";
+    cin >> a;
+    if (a % 2 == 0)
+    {
+        cout << "Its even!";
+    }
+    else
+    {
+        cout << "Its odd!";
+    }
+    return 0;
+}
