48b7bbf8f830ba782e00bc85078c097972cad75f
Harsha C K <harshack.21kumar@gmail.com>
2026-07-28 08:18:24 +0530

    Create 05_largest_of_two.cpp

---
+++ 05_largest_of_two.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "enter two numbers:";
+    cin >> a >> b;
+    if (a > b)
+    {
+        cout << "a is greater than b";
+    }
+    else if (b > a)
+    {
+        cout << "b is greater than a";
+    }
+    else
+    {
+        cout << "both are equal";
+    }
+    return 0;
+}
