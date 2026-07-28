d4da0780cc54021841f15b1b24895ed529143898
Harsha C K <harshack.21kumar@gmail.com>
2026-07-28 22:10:43 +0530

    Insert value into array and shift elements
    Insert value 30 into the array at position 2 and shift elements.

---
+++ 10_arrays.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10] = {10, 20, 40, 50};
+    int pos = 2;
+    int value = 30;
+    int n = 4;
+    for (int i = n; i > pos; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[pos] = value;
+    n++;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
