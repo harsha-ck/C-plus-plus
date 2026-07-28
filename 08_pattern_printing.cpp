0072e79514f46d2a3ccd48182f6b871a3c77daa1
Harsha C K <harshack.21kumar@gmail.com>
2026-07-28 10:17:06 +0530

    Update 08_pattern_printing.cpp

--- 08_pattern_printing.cpp
+++ 08_pattern_printing.cpp
@@ -52,3 +52,30 @@ int main()
     }
     return 0;
 }
+
+//pyramid
+int main()
+{
+    int n;
+    cout << "Enter the height of the pyramid: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
+
