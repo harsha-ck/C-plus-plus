24c8a774c5fb0f9337d3f6096de1a95aedaab40b
Harsha C K <harshack.21kumar@gmail.com>
2026-07-28 20:29:13 +0530

    Update 09_functions.cpp

--- 09_functions.cpp
+++ 09_functions.cpp
@@ -1,3 +1,4 @@
+//program 1
 #include <iostream>
 using namespace std;
 int factorial(int n);
@@ -19,4 +20,37 @@ int factorial(int n)
     {
         return n * factorial(n - 1);
     }
+}
+
+//program2
+#include <iostream>
+using namespace std;
+int prime_check(int n);
+int main()
+{
+    int result = prime_check(2);
+    if (result == 1)
+    {
+        cout << "It's prime!";
+    }
+    else
+    {
+        cout << "It's not prime!";
+    }
+}
+int prime_check(int n)
+{
+    if (n <= 1)
+    {
+        return -1;
+    }
+
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return -1;
+        }
+    }
+    return 1;
 }
