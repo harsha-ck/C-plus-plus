cf6b9fc1b053def1d1343f9b1ad4ef69b69b9736
Harsha C K <harshack.21kumar@gmail.com>
2026-07-28 19:09:06 +0530

    Update 08_pattern_printing.cpp

--- 08_pattern_printing.cpp
+++ 08_pattern_printing.cpp
@@ -79,3 +79,22 @@ int main()
     return 0;
 }
 
+//number triangle
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter height of the triangle:";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << j + 1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
