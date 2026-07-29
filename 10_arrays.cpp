d5d1512d0da003ec4f6da71e08251dd6937ff189
Harsha C K <harshack.21kumar@gmail.com>
2026-07-29 21:27:10 +0530

    Update 10_arrays.cpp

--- 10_arrays.cpp
+++ 10_arrays.cpp
@@ -69,3 +69,21 @@ int main()
 
     return 0;
 }
+
+//largest element
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+    int largest = arr[0];
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+    cout << largest;
+    return 0;
+}
