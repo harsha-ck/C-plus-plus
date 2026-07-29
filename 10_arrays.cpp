cd34a3388ef31adc7f4aafef826be40721949dcc
Harsha C K <harshack.21kumar@gmail.com>
2026-07-29 23:28:25 +0530

    Update 10_arrays.cpp

--- 10_arrays.cpp
+++ 10_arrays.cpp
@@ -211,3 +211,24 @@ int main()
     return 0;
 }
 
+//left rotation by one position
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int temp = arr[0];
+    for (int i = 0; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    arr[n - 1] = temp;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+
