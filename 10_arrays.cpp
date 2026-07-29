e8ec5e18beaf0c844f12fde61fe22cb2f625d781
Harsha C K <harshack.21kumar@gmail.com>
2026-07-29 22:06:00 +0530

    Update 10_arrays.cpp

--- 10_arrays.cpp
+++ 10_arrays.cpp
@@ -127,3 +127,28 @@ int main()
     }
     return 0;
 }
+
+// even and odd number check
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int count1 = 0;
+    int count2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count1++;
+        }
+        else
+        {
+            count2++;
+        }
+    }
+    cout << "Even count:" << count1 << endl;
+    cout << "Odd count:" << count2;
+    return 0;
+}
