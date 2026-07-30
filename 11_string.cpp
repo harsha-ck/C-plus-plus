9788c10ba581f28ed48769cbfd4e12d8170c7046
Harsha C K <harshack.21kumar@gmail.com>
2026-07-30 11:13:51 +0530

    Update 11_string.cpp

--- 11_string.cpp
+++ 11_string.cpp
@@ -13,3 +13,35 @@ int main()
     }
     return 0;
 }
+
+//vowels and consonants count
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    cout << "Enter a string:";
+    getline(cin, s);
+    int count1 = 0;
+    int count2 = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = tolower(s[i]);
+        if (ch >= 'a' && ch <= 'z')
+        {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                count1++;
+            }
+            else
+            {
+                count2++;
+            }
+        }
+    }
+    cout << "Vowels count:" << count1 << endl;
+    cout << "Consonants count:" << count2;
+    return 0;
+}
+
