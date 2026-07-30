7155807cbfdad2dc000ae74065eafdef434b6f2f
Harsha C K <harshack.21kumar@gmail.com>
2026-07-30 11:45:29 +0530

    Update 11_string.cpp

--- 11_string.cpp
+++ 11_string.cpp
@@ -70,4 +70,21 @@ int main()
     return 0;
 }
 
+//convert lowercase to uppercase
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    cout << "Enter a string:";
+    getline(cin, s);
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = toupper(s[i]);
+        cout << ch;
+    }
+    return 0;
+}
+
 
