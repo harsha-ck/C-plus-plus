d902604cf688fb165b21d2a91f1d9bf66a10272d
Harsha C K <harshack.21kumar@gmail.com>
2026-08-02 22:25:45 +0530

    Update 11_string.cpp

--- 11_string.cpp
+++ 11_string.cpp
@@ -228,6 +228,33 @@ int main()
     return 0;
 }
 
+//first non-repeating character
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    cout << "Enter a string:";
+    getline(cin, s);
+    int freq[256] = {0};
+    for (int i = 0; i < s.length(); i++)
+    {
+        freq[s[i]]++;
+    }
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (freq[s[i]] == 1)
+        {
+            cout << "First non-repeating character:" << s[i];
+            return 0;
+        }
+    }
+    cout << "No no-repeating character found!";
+    return 0;
+}
+
+
 
 
 
